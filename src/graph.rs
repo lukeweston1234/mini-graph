2e6d12a639f5238cf54fd93494dbf0716c6f22d2
Luke <luke.weston@cyberus-technology.de>
2025-07-04 15:20:11 +0200

    fixed graph sketch

--- src/graph.rs
+++ src/graph.rs
@@ -1,6 +1,11 @@
+
+// #![no_std]
+// extern crate alloc;
+
 use std::collections::VecDeque;
 
 use indexmap::IndexSet;
+
 
 pub enum GraphError {
     MaximumCapacity,
@@ -14,6 +19,7 @@ pub enum GraphError {
 pub trait Graph<N> {
     type Node;
     type Nid;
+    type Connections;
 
      fn with_capacity(capacity: usize) -> Self;
      fn add_node(&mut self, node:N) -> Self::Nid;
@@ -21,7 +27,7 @@ pub trait Graph<N> {
      fn add_edges(&mut self, edges: &[(Self::Nid, Self::Nid)]);
      fn topo_sort(&self) -> Result<Vec<usize>, GraphError>;
      fn get_node_mut(&mut self, index: usize) -> &mut N;
-     fn get_incoming(&self, index: usize) -> &IndexSet<usize>;
+     fn get_incoming(&self, index: usize) -> &Self::Connections;
 }
 
 /// A resizble graph for std environments, preferable for applications
@@ -37,6 +43,7 @@ pub struct DynamicGraph<N> {
 impl<N> Graph<N> for DynamicGraph<N> {
     type Nid = usize;
     type Node = N;
+    type Connections = IndexSet<Self::Nid>;
 
     fn with_capacity(capacity: usize) -> Self {
         Self {
@@ -61,11 +68,15 @@ impl<N> Graph<N> for DynamicGraph<N> {
         id
     }
     fn add_edge(&mut self, from: usize, to: usize) {
+        if (from == to) { return };
         self.outgoing[from].insert(to);
         self.incoming[to].insert(from);
     }
     fn add_edges(&mut self, edges: &[(usize, usize)]) {
         for (from, to) in edges {
+            if from == to {
+                continue
+            }
             self.outgoing[*from].insert(*to);
             self.incoming[*to].insert(*from);
         }
@@ -109,3 +120,108 @@ impl<N> Graph<N> for DynamicGraph<N> {
     }
 }
 
+// TODO: Fixed Size Graph for embedded environments
+
+// pub struct FixedGraph<const C: usize, N> {
+//     nodes: [N; C],
+//     incoming: [[usize; C]; C],
+//     outgoing: [[usize; C]; C],
+//     node_count: usize,
+// }
+
+// impl<const C: usize, N: Default> Graph<N> for FixedGraph<C, N> {
+//     type Nid = usize;
+//     type Node = N;
+//     type Connections = [usize; C];
+
+//     fn with_capacity(_capacity: usize) -> Self {
+//         Self {
+//             nodes: array::from_fn(|_| N::default()),
+//             incoming: [[0; C]; C],
+//             outgoing: [[0; C]; C],
+//             node_count: 0,
+//         }
+//     }
+
+//     fn add_node(&mut self, node: N) -> Self::Nid {
+//         if self.node_count >= C {
+//             panic!("cannot add node: reached fixed capacity {}", C);
+//         }
+//         let id = self.node_count;
+//         self.nodes[id] = node;
+//         self.node_count += 1;
+//         id
+//     }
+
+//     fn add_edge(&mut self, from: usize, to: usize) {
+//         if from == to { return; }
+//         // only allow within the range of added nodes
+//         if from < self.node_count && to < self.node_count {
+//             self.outgoing[from][to] = 1;
+//             self.incoming[to][from] = 1;
+//         } else {
+//             panic!("add_edge: node index out of bounds ({}, {})", from, to);
+//         }
+//     }
+
+//     fn add_edges(&mut self, edges: &[(usize, usize)]) {
+//         for &(from, to) in edges {
+//             if from == to { continue }
+//             if from < self.node_count && to < self.node_count {
+//                 self.outgoing[from][to] = 1;
+//                 self.incoming[to][from] = 1;
+//             } else {
+//                 panic!("add_edges: node index out of bounds ({}, {})", from, to);
+//             }
+//         }
+//     }
+
+//     fn topo_sort(&self) -> Result<Vec<usize>, GraphError> {
+//         let n = self.node_count;
+//         let mut indegree = [0usize; C];
+
+//         for u in 0..n {
+//             for v in 0..n {
+//                 if self.outgoing[u][v] != 0 {
+//                     indegree[v] += 1;
+//                 }
+//             }
+//         }
+
+//         let mut q = VecDeque::new();
+//         for i in 0..n {
+//             if indegree[i] == 0 {
+//                 q.push_back(i);
+//             }
+//         }
+
+//         let mut sorted = Vec::with_capacity(n);
+//         while let Some(u) = q.pop_front() {
+//             sorted.push(u);
+//             for v in 0..n {
+//                 if self.outgoing[u][v] != 0 {
+//                     indegree[v] -= 1;
+//                     if indegree[v] == 0 {
+//                         q.push_back(v);
+//                     }
+//                 }
+//             }
+//         }
+
+//         if sorted.len() == n {
+//             Ok(sorted)
+//         } else {
+//             Err(GraphError::CycleDetected)
+//         }
+//     }
+
+//     #[inline(always)]
+//     fn get_node_mut(&mut self, index: usize) -> &mut N {
+//         &mut self.nodes[index]
+//     }
+
+//     #[inline(always)]
+//     fn get_incoming(&self, index: usize) -> &Self::Connections {
+//         &self.incoming[index]
+//     }
+// }
