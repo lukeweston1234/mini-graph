82989046ff96f5535c6128f47256cfc037395b27
Luke <luke.weston@cyberus-technology.de>
2025-07-12 13:19:54 +0200

    draft at iter

--- src/mini_graph/bang.rs
+++ src/mini_graph/bang.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Bang {
     Bang,
     BangF32(f32),
--- src/nodes/bang/iter.rs
+++ src/nodes/bang/iter.rs
@@ -14,19 +14,18 @@ impl<'a, const C: usize, const N: usize> Iter<'a, C, N> {
 }
 impl<'a, const C: usize, const N: usize> Node<C, N> for Iter<'a, C, N>{
     fn handle_bang(&mut self, inputs: &[Bang], output: &mut Bang) {
-        if let Some(input) = inputs.get(0) {
-            match input {
-                Bang::Bang => {
-                    *output = self.values[self.position];
-                    if self.position < self.values.len() - 1 {
-                        self.position += 1;
-                    }
-                    else {
-                        self.position = 0;
-                    }
-                },
-                _ => ()
+        for input in inputs {
+            if *input == Bang::Bang {
+                *output = self.values[self.position];
+                if self.position < self.values.len() - 1 {
+                    self.position += 1;
+                }
+                else {
+                    self.position = 0;
+                }
+                return
             }
         }
+
     }
 }
